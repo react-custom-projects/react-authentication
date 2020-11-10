f98e377fba50b58f4737beffcbc96dd8a107cec2
Adam Morsi <adam.morsi@yahoo.com>
2020-11-10 12:45:25 +0200

    add function to decode token

--- src/js/constants/helper.js
+++ src/js/constants/helper.js
@@ -1,4 +1,8 @@
+//toast
+import { toast } from 'react-toastify';
+//constants
 import { createBrowserHistory } from 'history';
+import { getSignInPageUrl } from './AppUrls';
 
 export const history = createBrowserHistory();
 
@@ -6,4 +10,19 @@ export const history = createBrowserHistory();
 export const objectWithoutKey = (object, key) => {
 	const { [key]: deletedKey, ...otherKeys } = object;
 	return otherKeys;
+};
+
+//decode token
+export const getUserFromJwtToken = (token) => {
+	try {
+		const jwtPayload = JSON.parse(atob(token.split('.')[1])),
+			//revise this line
+			user = { ...jwtPayload.user, per: jwtPayload.per };
+		console.log(jwtPayload);
+		return user;
+	} catch (err) {
+		history.push(getSignInPageUrl());
+		toast.error('Something went wrong');
+		return {};
+	}
 };
